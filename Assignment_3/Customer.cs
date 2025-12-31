08e47f4b7742e3b3b74558d80657dfa5522ec002
deepanshu khandelwal <deepanshu.k@intimetec.com>
2025-12-31 11:37:42 +0530

    assignment 3 of functions

---
+++ Assignment_3/Customer.cs
@@ -0,0 +1,7 @@
+public class Customer
+{
+    public string ID { get; set; }
+    public string Company { get; set; }
+    public string Contact { get; set; }
+    public string Country { get; set; }
+}
